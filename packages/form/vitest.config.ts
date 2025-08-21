66652dd1fb747ee07876cdb55421c803f33f30f5
better-salmon <132773930+better-salmon@users.noreply.github.com>
2025-08-21 13:12:16 +0300

    Refactor Vitest configurations

---
+++ packages/form/vitest.config.ts
@@ -0,0 +1,21 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vitest/config";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@form": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    typecheck: {
+      enabled: true,
+      include: ["src/**/*.test-d.ts"],
+      tsconfig: path.resolve(__dirname, "./tsconfig.lib.json"),
+    },
+    include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+  },
+});
